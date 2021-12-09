a2151cc27c55dea02fde7f64d5ea6b39491d3bfd
Manjay <manjaypoddar123@gmail.com>
2021-12-09 14:34:57 +0530

    updated files

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { View, Button, StyleSheet, Text } from "react-native";
+import analytics from "@react-native-firebase/analytics";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Button
+        title="Add To Basket"
+        onPress={async () =>
+          await analytics().logEvent("basket", {
+            item: "mens grey t-shirt",
+          })
+        }
+      />
     </View>
   );
 }
@@ -14,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
